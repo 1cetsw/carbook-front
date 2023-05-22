f2944e9cff2c90b369614c36352b863b9e75a94f
1cetsw <1cetsw@gmail.com>
2023-05-22 17:16:18 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import carbook from './carbook.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"./carbook.png"} className="App-logo" alt="logo" />
-        <p>
-          CarBook
-        </p>
+        <img src={carbook} className="App-logo" alt="logo" />
+        
         
       </header>
     </div>
