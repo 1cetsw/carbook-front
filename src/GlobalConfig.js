dc3f35cc7c862426a210e9e5da34d83e3692b686
bartekk1111 <bartosz_keskrawiec@wp.pl>
2023-06-17 08:10:13 +0200

    Still testing dev

--- src/GlobalConfig.js
+++ src/GlobalConfig.js
@@ -1,7 +1,7 @@
 //use global.config.HostApi
 module.exports = global.config = {
-    HostApi: "https://carbook-api.up.railway.app",
-    // HostApi: "http://localhost:8080",
+    // HostApi: "https://carbook-api.up.railway.app",
+    HostApi: "http://localhost:7059",
     HostFront: "https://carbook-front-production.up.railway.app",
     // HostFront: "http://localhost:3000",
 };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://carbook-api.up.railway.app",
+      target: "http://localhost:8080",
       changeOrigin: true,
     })
   );
