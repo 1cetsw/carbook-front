3b56e9ed1ca0fd61951d1430a6cffaa71a16e6eb
1cetsw <1cetsw@gmail.com>
2023-06-18 16:30:55 +0100

    delete old folder

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      // target: "http://localhost:8080",
-      target: "https://carbook-api.up.railway.app",
+      target: "http://localhost:8080",
+      // target: "https://carbook-api.up.railway.app",
       changeOrigin: true,
     })
   );
