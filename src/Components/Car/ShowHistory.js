869e7611a65f3cfd206ae5866e78bc33e9fa1c0f
bartekk1111 <bartosz_keskrawiec@wp.pl>
2023-06-28 23:18:26 +0200

    Fetch in car history

--- src/Components/Car/ShowHistory.js
+++ src/Components/Car/ShowHistory.js
@@ -1,9 +1,23 @@
 
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import {useLocation} from 'react-router-dom';
 
 const ShowHistory = () => {
+    const location = useLocation();
+    const carId = location.state.carId;
+    const [services, setServices] = useState([]);
 
+    useEffect(() => {
+        fetch(global.config.HostFront + '/api/cars/car-services/' + carId)
+            .then(response => response.json())
+            .then(data => {
+                setServices(data);
+                console.log(data);
+            })
+            .catch(error => {
+                console.log('Error fetching data:', error);
+            });
+    }, [carId]);
 
     return (
         <div className="container">
