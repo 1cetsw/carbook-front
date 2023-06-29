166e56f330d057414fd9182e770ffe92b6eb896d
1cetsw <1cetsw@gmail.com>
2023-06-29 12:37:00 +0100

    add Last service(check date) and add history ,  install react-boostrap

--- src/Components/Car/CarDetails.js
+++ src/Components/Car/CarDetails.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import {Link, useLocation} from 'react-router-dom';
-import {Container, Alert} from 'react-bootstrap';
+import {Alert} from 'react-bootstrap';
 
 const CarDetails = () => {
     const location = useLocation();
