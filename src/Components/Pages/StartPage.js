e989cd102446faf1084bf576879790eda0e7293a
1cetsw <1cetsw@gmail.com>
2023-07-07 18:19:32 +0100

    add validate password

--- src/Components/Pages/StartPage.js
+++ src/Components/Pages/StartPage.js
@@ -6,20 +6,18 @@ const StartPage = () => {
 
     
         return (
-
             <div>
-
                 <header className="py-5 bg-image-full" style={{
                     backgroundImage: `url(${background})`,
                     backgroundRepeat: "no-repeat",
                     backgroundSize: "cover"
                 }}>
-
                     <div className="text-center my-5">
                         <img className="img-fluid rounded-circle mb-4" src={carbookLogo} alt="carbook logo" />
                         <h1 className="text-white fs-3 fw-bolder">All your vehicles in one place</h1>
                         <p className="text-white-50 mb-0">Do you also not remember when you changed the oil,<br />
-                            timing belt or air filter in your car? <br />Soon you won't have to, we'll do it for you.</p>
+                            timing belt or air filter in your car?
+                            <br />Soon you won't have to, we'll do it for you.</p>
                     </div>
                 </header>
                 <footer className="py-5 bg-dark">
